40f7e94b21e7a72c5c72fc5b592001afbe99c9cd
Barima Kwarteng <bakwarte@gmail.com>
2012-05-18 09:05:34 -0600

    Initial Commit

---
+++ BID/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BID
+//
+//  Created by Barima Kwarteng on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BID/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BID
+//
+//  Created by Barima Kwarteng on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
